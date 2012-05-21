9b2303000401928983bb13dcc89fa227c2b3d24f
Marek Wiewiorski <marek@mwicat.com>
2012-05-21 10:33:49 +0200

    busytone fix

--- src/mod/endpoints/mod_skinny/mod_skinny.c
+++ src/mod/endpoints/mod_skinny/mod_skinny.c
@@ -798,7 +798,7 @@ int channel_on_hangup_callback(void *pArg, int argc, char **argv, char **columnN
 		skinny_line_set_state(listener, line_instance, call_id, SKINNY_ON_HOOK);
 		send_select_soft_keys(listener, line_instance, call_id, SKINNY_KEY_SET_ON_HOOK, 0xffff);
 		send_define_current_time_date(listener);
-		send_set_speaker_mode(listener, SKINNY_SPEAKER_OFF);
+		/* send_set_speaker_mode(listener, SKINNY_SPEAKER_OFF); */
 		send_set_ringer(listener, SKINNY_RING_OFF, SKINNY_RING_FOREVER, 0, call_id);
 	}
 	return 0;
